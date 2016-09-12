c7c76beddeee3478321859025d8936e8d681e211
Brady Vaughan <bradytvaughan@gmail.com>
2016-09-12 11:30:09 -0700

    added the four interesting rules to the toggle, made the reset button work, except that it does not reset the state of the automaton.

--- entry.js
+++ entry.js
@@ -7,6 +7,8 @@ window.Row = Row;
 window.init = () => {
   const ruleThirty = {'100': 1, '011': 1, '010': 1, '001': 1}
   const ruleNinety = {'100': 1, '011': 1, '110': 1, '001': 1}
+  const ruleOneTen = {'001': 1, '011': 1, '110': 1, '101': 1, '010': 1}
+  const ruleOneEightFour = {'111': 1, '011': 1, '101': 1, '100': 1}
   let start = new Array(51)
   start[26] = 1
   window.x = new Row(51, ruleNinety, start);
@@ -24,7 +26,7 @@ window.init = () => {
   })
 
   $('#reset').on("click", () => {
-    alert('reset!');
+    $('.grid').empty();
   })
 
   $('#rule').on("click", () => {
@@ -32,10 +34,28 @@ window.init = () => {
       let start = new Array(51)
       start[26] = 1
       window.x = new Row(51, ruleNinety, start);
+      $('#note').text('Current Rule: Rule90')
+    } else if (x.auto[0].rule === ruleNinety) {
+      let start = new Array(51)
+      start[50] = 1
+      window.x = new Row(51, ruleOneTen, start);
+      $('#note').text('Current Rule: Rule110')
+    } else if (x.auto[0].rule === ruleOneTen) {
+      let star = [];
+      for (let i = 0; i < 51; i++) {
+        if (Math.random() > .5) {
+          star.push(1);
+        } else {
+          star.push(0);
+        }
+      }
+      window.x = new Row(51, ruleOneEightFour, star);
+      $('#note').text('Current Rule: Rule184')
     } else {
       let start = new Array(51)
       start[26] = 1
       window.x = new Row(51, ruleThirty, start);
+      $('#note').text('Current Rule: Rule30')
     }
   })
 }
