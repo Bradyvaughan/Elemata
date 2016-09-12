81133e921bdd6df02cbdc17c7eb9c0a45ae67ef2
Brady Vaughan <bradytvaughan@gmail.com>
2016-09-12 10:57:47 -0700

    converted the rule button into a toggle

--- entry.js
+++ entry.js
@@ -5,9 +5,11 @@ window.Cell = Cell;
 window.Row = Row;
 
 window.init = () => {
+  const ruleThirty = {'100': 1, '011': 1, '010': 1, '001': 1}
+  const ruleNinety = {'100': 1, '011': 1, '110': 1, '001': 1}
   let start = new Array(51)
   start[26] = 1
-  window.x = new Row(51, {'100': 1, '011': 1, '110': 1, '001': 1}, start);
+  window.x = new Row(51, ruleNinety, start);
 
   $('#start').on('click', () => {
     if (!x.int) {
@@ -26,9 +28,14 @@ window.init = () => {
   })
 
   $('#rule').on("click", () => {
-    let start = new Array(51)
-    start[26] = 1
-    window.x = new Row(51, {'100': 1, '011': 1, '010': 1, '001': 1}, start);
-    debugger;
+    if (x.auto[0].rule === ruleThirty) {
+      let start = new Array(51)
+      start[26] = 1
+      window.x = new Row(51, ruleNinety, start);
+    } else {
+      let start = new Array(51)
+      start[26] = 1
+      window.x = new Row(51, ruleThirty, start);
+    }
   })
 }
