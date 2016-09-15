19d390b0ac13fd18b47a5c8ca7ff93a07ca510e3
Brady Vaughan <bradytvaughan@gmail.com>
2016-09-15 12:11:29 -0700

    continued to style

--- entry.js
+++ entry.js
@@ -233,12 +233,14 @@ window.init = () => {
       window.colors = [];
       $('#rainbow').text('Rainbow Mode')
       $('.header').css('background-image','url(circuits.jpg)')
+      $('button').css('background-color','#cff')
     } else {
       window.colors = ['f00', 'f40', 'f80', 'fc0', 'ff0', 'cf0', '8f0', '4f0',
         '0f0', '0f4', '0f8', '0fc', '0ff', '0cf', '08f', '04f', '00f', '40f',
         '80f', 'c0f', 'f0f', 'f0c', 'f08', 'f04']
       $('#rainbow').text('Monochrome')
       $('.header').css('background-image','url(rainbow_circuits.jpg)')
+      $('button').css('background-color','#ccc')
     }
   })
 }
