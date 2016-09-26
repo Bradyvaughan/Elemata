8125acee166ef6469e50c0c8bdec79095d16c1f7
Brady Vaughan <bradytvaughan@gmail.com>
2016-09-26 10:48:21 -0700

    file curation

--- bundle.js
+++ bundle.js
@@ -290,12 +290,12 @@
 	    if (colors[0]) {
 	      window.colors = [];
 	      $('#rainbow').text('Rainbow Mode');
-	      $('.header').css('background-image', 'url(circuits.jpg)');
+	      $('.header').css('background-image', 'url(images/circuits.jpg)');
 	      $('button').css('background-color', '#cff');
 	    } else {
 	      window.colors = ['f00', 'f40', 'f80', 'fc0', 'ff0', 'cf0', '8f0', '4f0', '0f0', '0f4', '0f8', '0fc', '0ff', '0cf', '08f', '04f', '00f', '40f', '80f', 'c0f', 'f0f', 'f0c', 'f08', 'f04'];
 	      $('#rainbow').text('Monochrome');
-	      $('.header').css('background-image', 'url(rainbow_circuits.jpg)');
+	      $('.header').css('background-image', 'url(images/rainbow_circuits.jpg)');
 	      $('button').css('background-color', '#ccc');
 	    }
 	  });
